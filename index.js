9829cfd19fddaaf11569e11f3f75111491d04d28
C-a-n-j <eddymacharia450@gmail.com>
2024-04-08 12:28:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+//bestcustomer
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+//overriden bestcustomer
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+const favCustomer = 'Kev'
+
+function changeLeastFavoriteCustomer(){
+    favCustomer = 'bob'
+    if(favCustomer === bob){
+        console.log(`Your favorite customer is ${favCustomer}`)
+    }else{
+        console.log('Assignment to constant variable.')
+    }
+}
